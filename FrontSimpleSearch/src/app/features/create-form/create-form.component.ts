3e109168c4719a58766f193f37fc682e0103b5f2
vinic <viniciusrbastos@hotmail.com>
2025-02-04 14:20:18 -0300

    Estruturando arquitetura e configuração do projeto.

---
+++ FrontSimpleSearch/src/app/features/create-form/create-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-form',
+  imports: [],
+  templateUrl: './create-form.component.html',
+  styleUrl: './create-form.component.scss'
+})
+export class CreateFormComponent {
+
+}
