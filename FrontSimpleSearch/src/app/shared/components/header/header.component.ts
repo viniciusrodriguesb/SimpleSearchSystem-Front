824c14021c8639f095e5fd80278ae4bd9f3892da
vinic <viniciusrbastos@hotmail.com>
2025-02-11 12:37:02 -0300

    Feat: ajustando header, footer e pagina inicial.

--- FrontSimpleSearch/src/app/shared/components/header/header.component.ts
+++ FrontSimpleSearch/src/app/shared/components/header/header.component.ts
@@ -4,8 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
